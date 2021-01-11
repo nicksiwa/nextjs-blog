0dd443b1240ffc836cd042f3e2b91e88a6ff6b9c
NATTAPORN AUTTAKUL (ณัฎฐาพร อุทกูล) <5830214038@email.psu.ac.th>
2021-01-12 00:44:29 +0700

    add pagination path

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,5 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import { getSortedPostsData } from "../lib/posts";
-import { Content } from "../components/section";
-import { List, ListItem } from "../components/content";
+import Home from "./posts/page/[page]";
+import { getStaticProps } from "./posts/page/[page]";
 
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-
-      <Content md>
-        <p>Hello, I am a front-end developer 😁</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </Content>
-
-      <Content md style={{ paddingTop: "10px" }}>
-        <List>
-          {allPostsData.map((post) => (
-            <ListItem post={post} key={post.id} />
-          ))}
-        </List>
-      </Content>
-    </Layout>
-  );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
+export { getStaticProps };
+export default Home;
---
+++ pages/posts/page/[page].js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../../../components/layout";
+import { getPostPagination, getSortedPostsData } from "../../../lib/posts";
+import { Content } from "../../../components/section";
+import { List, ListItem } from "../../../components/content";
+import { POSTS_PER_PAGE } from "../../../constants/app";
+
+export default function Home({ posts, numPages, currentPage }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <Content md>
+        <p>Hello, I am a front-end developer 😁</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </Content>
+
+      <Content md style={{ paddingTop: "10px" }}>
+        <List>
+          {posts.map((post) => (
+            <ListItem post={post} key={post.id} />
+          ))}
+        </List>
+      </Content>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const pages = getPostPagination();
+
+  return {
+    paths: pages,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const page = (params && parseInt(params.page)) || 1;
+  const allPostsData = getSortedPostsData();
+  const numPages = Math.ceil(allPostsData.length / POSTS_PER_PAGE);
+  const pageIndex = page - 1;
+  const posts = allPostsData.slice(
+    pageIndex * POSTS_PER_PAGE,
+    (pageIndex + 1) * POSTS_PER_PAGE
+  );
+
+  return {
+    props: {
+      posts,
+      numPages,
+      currentPage: page,
+    },
+  };
+}
